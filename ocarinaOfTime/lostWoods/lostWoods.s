c363961e017d734948ae2fcf89904e1144a567ca
ZerotoKoops <zerotokoops@gmail.com>
2026-08-19 00:17:37 -0700

    Updated Lost Woods

--- ocarinaOfTime/lostWoods/lostWoods.s
+++ ocarinaOfTime/lostWoods/lostWoods.s
@@ -1,478 +1,572 @@
-lostWoodsStart:
-; BPM = 138.46, B = 26, M = 104
-lostWoodsChannel1:
-.redefine BEAT 1
-.redefine NO_FIRST_VOL 0
+musLostWoodsStart:
+    tempo 140
+musLostWoodsChannel1:
+.redefine HI_VOL $6
+.redefine LO_VOL $4
 ; Measure 1
-; rest
     vol $0
-    beat f3 104
+    beat gs3 W
 
-lostWoodsChannel1Measure2Reset:
-    resetCoda
-lostWoodsChannel1Measure2:
-    incCoda
-; Measure 2
+musLostWoodsChannel1Measure2Loop:
+.rept 2 INDEX REPTCTR
+; Measure 2,6
     octave 5
     duty $02
     env $0 $05
-    vol $6
+    vol HI_VOL
 .rept 2
-    beat f 13 a 13 b 26
+    beat f E1 a E2 b Q
 .endr
-; Measure 3
-    beat f 13 a 13 b 13 ou e 13
-    beat d 26 od b 13 ou c 13
+; Measure 3,7
+    beat f E1 a E2 b E1 ou e E2
+    beat d Q od b E1 ou c E2
 
-    gotoCond 2 lostWoodsChannel1Measure8
+.ifeq REPTCTR 0
 ; Measure 4
     octaved
-    beat b 13 g 13
+    beat b E1 g E2
 
     env $0 $00
     vibrato $62
-    beat e 52 r 13
+    beat e HF r E1
 
     env $0 $05
     vibrato $00
-    beat d 13
+    beat d E2
 ; Measure 5
-    beat e 13 g 13
+    beat e E1 g E2
 
     env $0 $00
     vibrato $82
-    beat e 65 r 13
+    beat e HF+E1 r E2
+.endif
+.endr
 
-    goto lostWoodsChannel1Measure2
-
-lostWoodsChannel1Measure8:
 ; Measure 8
-    octaveu
-    beat e 13 od b 13
+    octave 6
+    beat e E1 od b E2
 
     env $0 $00
     vibrato $62
-    beat g 52 r 13  
+    beat g HF r E1  
 
     env $0 $02
     vibrato $00 
-    beat b 13
+    beat b E2
 ; Measure 9
     env $0 $05
-    beat g 13 d 13
+    beat g E1 d E2
 
     env $0 $00
     vibrato $82
-    beat e 65 r 13
+    beat e HF+E1 r E2
 
-lostWoodsChannel1Measure10:
-; Coda 2
-    incCoda
+.rept 2 INDEX REPTCTR
 ; Measure 10,14
-    octaved
+    octave 4
     vibrato $00
     env $1 $04
-	vol $8
-    beat d 13 e 13 f 26
-    beat g 13 a 13 b 26
+	vol HI_VOL+2
+    beat d E1 e E2 f Q
+    beat g E1 a E2 b Q
 ; Measure 11,15
     octaveu
-    beat c 13 od b 13
+    beat c E1 od b E2
     
     env $1 $00
     vibrato $62
-    beat e 65 r 13
+    beat e HF+E1 r E2
 
-    gotoCond 4 lostWoodsChannel1Measure16
+.ifeq REPTCTR 0
 ; Measure 12
     octaveu
     vibrato $00
     env $0 $05
-    vol $5
-    beat f 13 g 7 r 6
-    vol $6
-    beat a 26
-    vol $7
-    beat b 13 ou c 7 r 6
-    vol $8
-    beat d 26
+    vol HI_VOL-1
+    beat f E1 g S3 r S4
+    vol HI_VOL
+    beat a Q
+    vol HI_VOL+1
+    beat b E1 ou c S3 r S4
+    vol HI_VOL+2
+    beat d Q
 ; Measure 13
-    beat e 13 f 7 r 6
+    beat e E1 f S3 r S4
 
     env $0 $00
     vibrato $81
-    beat g 65 r 13
+    beat g HF+E1 r E2
 
-    vol $6
-    goto lostWoodsChannel1Measure10
+    vol HI_VOL
+.endif
+.endr
 
-lostWoodsChannel1Measure16:
 ; Measure 16
-	octaved
+	octave 5
     vibrato $00
     env $0 $05
-    vol $5
-    beat f 13 e 7 r 6
-    beat a 13 g 7 r 6
-    vol $6
-    beat b 13 a 7 r 6
-    beat ou c 13 od b 7 r 6
+    vol HI_VOL-1
+    beat f E1 e S3 r S4
+    beat a E1 g S3 r S4
+    vol HI_VOL
+    beat b E1 a S3 r S4
+    beat ou c E1 od b S3 r S4
 ; Measure 17
     octaveu
-	vol $7
-    beat d 13 od b 7 r 6
-    beat ou e 13 d 7 r 6
-	vol $8
-    beat f 13 e 7 r 6
-    beat e 7 f 6
-    beat r 6 d 7
+	vol HI_VOL+1
+    beat d E1 od b S3 r S4
+    beat ou e E1 d S3 r S4
+	vol HI_VOL+2
+    beat f E1 e S3 r S4
+    beat e S1 f S2 r S3 d S4
 ; Measure 18-19
 	env $0 $00
 	vibrato $e2
-    beat e 65 
-	vol $6
+    beat e HF+E1 
+	vol HI_VOL
 	vibrato $02
-	beat e 26
-	vol $3
-	beat e 13 r 78
+	beat e E2+E1
+	vol LO_VOL
+	beat e E2 r HF+Q
 
 	octaved
-	vol $8
+	vol HI_VOL+2
 	env $0 $05
 	vibrato $00
-	beat b 10 r 16
+	beat b S1+T3 r T4+E2
 
-    goto lostWoodsChannel1Measure2Reset
+    goto musLostWoodsChannel1Measure2Loop
     cmdff
 
 
 
-lostWoodsChannel0:
-.redefine BEAT 1
-; Measure 1
-; rest
-    vol $0
-    beat f3 117     ;104+13
-
-lostWoodsChannel0Measure2Reset:
-    resetCoda
-lostWoodsChannel0Measure2:
-    incCoda
-; Measure 2-3,6-7
+musLostWoodsChannel0:
 .redefine HI_VOL $5
 .redefine LO_VOL $2
-.redefine LO_VOL_RATIO 6/13
+; Measure 1
+    vol $0
+    beat f3 W+E1
+
+musLostWoodsChannel0Measure2Loop:
     octave 3
     duty $03
     env $0 $00
-    volbeat a 13 a 13 a 13 r 13
-
-    gotoCond 4 lostWoodsChannel0Measure4
-    gotoCond 4+8 lostWoodsChannel0Measure4
-    goto lostWoodsChannel0Measure2
-
-lostWoodsChannel0Measure4:
-    incCoda
-; Measure 4-5,8-9
-    volbeat g 13 g 13 g 13 r 13
-
-    gotoCond 4+4 lostWoodsChannel0Measure2
-    gotoCond 4+12 lostWoodsChannel0Measure10
-    goto lostWoodsChannel0Measure4
-
-lostWoodsChannel0Measure10:
-; Coda 16
-    incCoda
-; Measure 10
 .rept 2
-    vol $5
-    beat f 7
-    vol $2
-    beat a 6 r 13
+.rept 4
+; Measure 2a-3,6a-7
+    ;beat a 13 a 13 a 13 r 13
+    vol HI_VOL
+    beat a S3
+    vol LO_VOL
+    beat a S4
+
+    vol HI_VOL
+    beat a S1
+    vol LO_VOL
+    beat a S2
+
+    vol HI_VOL
+    beat a S3
+    vol LO_VOL
+    beat a S4 r E1
+.endr
+.rept 4
+; Measure 4-5,8-9
+    ;beat g 13 g 13 g 13 r 13
+    vol HI_VOL
+    beat g S3
+    vol LO_VOL
+    beat g S4
+
+    vol HI_VOL
+    beat g S1
+    vol LO_VOL
+    beat g S2
+
+    vol HI_VOL
+    beat g S3
+    vol LO_VOL
+    beat g S4 r E1
+.endr
+.endr
+
+.rept 2 INDEX REPTCTR
+; Measure 10
+    octave 3
+.rept 2
+    vol HI_VOL
+    beat f S3
+    vol LO_VOL
+    beat a S4 r E1
 .endr
     octaved
 .rept 2
-    vol $5
-    beat ou d 7
-    vol $2
-    beat od g 6 r 13
+    vol HI_VOL
+    beat ou d S3
+    vol LO_VOL
+    beat od g S4 r E1
 .endr
 ; Measure 11
     octaveu
 .rept 2
-    vol $5
-    beat g 7
-    vol $2
-    beat c 6 r 13
+    vol HI_VOL
+    beat g S3
+    vol LO_VOL
+    beat c S4 r E1
 .endr
     octaved
-    vol $5
-    beat ou e 7
-    vol $2
-    beat od a 6 r 13
-    vol $5
-    beat ou e 7
-    vol $2
-    beat od a 6
+    vol HI_VOL
+    beat ou e S3
+    vol LO_VOL
+    beat od a S4 r E1
+    vol HI_VOL
+    beat ou e S3
+    vol LO_VOL
+    beat od a S4
 
-    gotoCond 2+16 lostWoodsChannel0Measure16
+.ifeq REPTCTR 0
 ; Measure 12
     octave 4
     vibrato $00
     duty $02
     env $1 $04
-    vol $5
-    beat d 13 e 7 r 6
-    vol $6
-    beat f 26
-    vol $7
-    beat g 13 a 7 r 6
-    vol $8
-    beat b 26
+    vol HI_VOL
+    beat d E1 e S3 r S4
+    vol HI_VOL+1
+    beat f Q
+    vol HI_VOL+2
+    beat g E1 a S3 r S4
+    vol HI_VOL+3
+    beat b Q
 
 ; Measure 13
     octaveu
-    beat c 13 d 7 r 6
+    beat c E1 d S3 r S4
 
     env $1 $00
     vibrato $62
-    beat e 65
+    beat e HF+E1
 
     octave 3
     vibrato $00
     duty $03
     env $0 $00
-    vol $5
-    beat e 7
-    vol $2
-    beat od a 6 r 13
+    vol HI_VOL
+    beat e S3
+    vol LO_VOL
+    beat od a S4 r E1
+.endif
+.endr
 
-    goto lostWoodsChannel0Measure10
-
-lostWoodsChannel0Measure16:
 ; Measure 16
 	octave 4
     duty $02
     env $1 $04
-    vol $5
-    beat d 13 c 7 r 6
-    beat f 13 e 7 r 6
-    vol $6
-    beat g 13 f 7 r 6
-    beat a 13 g 7 r 6
+    vol HI_VOL
+    beat d E1 c S3 r S4
+    beat f E1 e S3 r S4
+    vol HI_VOL+1
+    beat g E1 f S3 r S4
+    beat a E1 g S3 r S4
 ; Measure 17
-	vol $7
-    beat b 13 a 7 r 6
-    beat ou c 13 od b 7 r 6
-	vol $8
-    beat ou d 13 c 7 r 6
-    beat od b 7 ou c 12 od a 7
+	vol HI_VOL+2
+    beat b E1 a S3 r S4
+    beat ou c E1 od b S3 r S4
+	vol HI_VOL+3
+    beat ou d E1 c S3 r S4
+    beat od b S1 ou c S2+S3 od a S4
 ; Measure 18
     env $1 $00
     vibrato $e2
-    beat b 65 
-	vol $6
+    beat b HF+E1 
+	vol HI_VOL+1
+    env $0 $00
 	vibrato $02
-	beat b 26
-	vol $3
-	beat b 13 r 13
+	beat b E2+E1
+	vol LO_VOL+1
+	beat b E2 r E1
 ; Measure 19a
 	octave 3
-    env $1 $04
 	vibrato $00
-	volbeat gs 13 gs 13 gs 13
-	volbeat gs 13 r 13
-    vol $8
-    beat ou e 10 r 29     ;3+13+13
+	;beat gs 13 gs 13 gs 13 g 13 r 13
+    vol HI_VOL
+    beat gs S1
+    vol LO_VOL
+    beat gs S2
 
-    goto lostWoodsChannel0Measure2Reset
+    vol HI_VOL
+    beat gs S3
+    vol LO_VOL
+    beat gs S4
+
+    vol HI_VOL
+    beat gs S1
+    vol LO_VOL
+    beat gs S2   
+
+    vol HI_VOL
+    beat gs S3
+    vol LO_VOL
+    beat gs S4 r E2
+
+    vol HI_VOL+3
+    beat ou e S1+T3 r T4+E2+E1
+
+    goto musLostWoodsChannel0Measure2Loop
     cmdff
 
 
 
-lostWoodsChannel4:
-.redefine BEAT 1
-    beat r 104
-
-lostWoodsChannel4Measure2Reset:
-    resetCoda
-lostWoodsChannel4Measure2:
-.redefine CHANNEL 4
+musLostWoodsChannel4:
 .redefine HI_VOL $0e
 .redefine LO_VOL $0f
-.redefine LO_VOL_RATIO 6/13
-; Coda 0,
-    incCoda
+.redefine HI_VOL2 $15
+.redefine LO_VOL2 $14
+
+; Measure 1
+    beat r W
+
+musLostWoodsChannel4Measure2Loop:
 ; Measure 2-3,6-7
     octave 3
+.rept 2
+.rept 4
+    duty HI_VOL
+    beat f E1
+	;beat ou c 13 c 13 c 13
+    octaveu
+    duty HI_VOL2
+    beat c S3
+    duty LO_VOL2
+    beat c S4
 
-    duty $0e
-    beat f 13
-.redefine NO_FIRST_VOL 1
-	volbeat ou c 13
-.redefine NO_FIRST_VOL 0
-	volbeat c 13 c 13
+    duty HI_VOL2
+    beat c S1
+    duty LO_VOL2
+    beat c S2
 
-    gotoCond 4 lostWoodsChannel4Measure4
-    gotoCond 4+6 lostWoodsChannel4Measure4
-    goto lostWoodsChannel4Measure2
+    duty HI_VOL2
+    beat c S3
+    duty LO_VOL2
+    beat c S4
 
-lostWoodsChannel4Measure4:
-; Coda 4
-    incCoda
+    octaved
+.endr
+.rept 4 INDEX REPTCTR
 ; Measure 4-5,8-9
+    duty HI_VOL
+    beat e E1
+	;beat ou c 13 c 13 c 13
+    octaveu
+    duty HI_VOL2
+    beat c S3
+    duty LO_VOL2
+    beat c S4
+
+    duty HI_VOL2
+    beat c S1
+    duty LO_VOL2
+    beat c S2
+.ifeq (REPTCTR # 2) 0
+    duty HI_VOL2
+    beat c S1
+    duty LO_VOL2
+    beat c S2
     octaved
-
-	duty $0e
-    beat e 13
-.redefine NO_FIRST_VOL 1
-	volbeat ou c 13
-.redefine NO_FIRST_VOL 0
-	volbeat c 13 c 13
-
+.else 
     octaved
-	duty $0e
-    beat e 13
-.redefine NO_FIRST_VOL 1
-	volbeat ou c 13
-	duty $0e
-	volbeat c 13
+	duty HI_VOL
+    beat c E2
+.endif
+.endr
+.endr
 
-    octaved
-	duty $0e
-    beat c 13
-
-    gotoCond 2+4 lostWoodsChannel4Measure2
-    gotoCond 2+10 lostWoodsChannel4Measure10
-    goto lostWoodsChannel4Measure4
-
-lostWoodsChannel4Measure10:
-; Coda 12
-    incCoda
-; Measure 10
+.rept 3
+; Measure 10,12,14
 .rept 2
-    volbeat d 13
-.redefine NO_FIRST_VOL 0
-	volbeat a 13
+    duty HI_VOL
+    beat d S1
+    duty LO_VOL
+    beat d S2
+
+	duty HI_VOL
+    beat a S3
+    duty LO_VOL
+    beat a S4
 .endr
 .rept 2
-    volbeat od g 13 ou g 13
+    octaved
+    duty HI_VOL
+    beat g S1
+    duty LO_VOL
+    beat g S2
+
+    octaveu
+	duty HI_VOL
+    beat g S3
+    duty LO_VOL
+    beat g S4
 .endr
-; Measure 11
+; Measure 11,13,15
     octaveu
 .rept 2
-    volbeat od c 13 ou c 13
+    octaved
+    duty HI_VOL
+    beat c S1
+    duty LO_VOL
+    beat c S2
+
+    octaveu
+	duty HI_VOL
+    beat c S3
+    duty LO_VOL
+    beat c S4
 .endr
 	octaved
 .rept 2
-    volbeat od a 13 ou a 13
+    octaved
+    duty HI_VOL
+    beat a S1
+    duty LO_VOL
+    beat a S2
+
+    octaveu
+	duty HI_VOL
+    beat a S3
+    duty LO_VOL
+    beat a S4
+.endr
 .endr
 
-    gotoCond 3+12 lostWoodsChannel4Measure16
-    goto lostWoodsChannel4Measure10
-
-lostWoodsChannel4Measure16:
-.redefine LO_VOL_RATIO 3/13
 ; Measure 16
 .rept 2
-	duty $0e
-    beat d 13
-.redefine NO_FIRST_VOL 1
-	volbeat a 13
-	duty $0e
-	volbeat a 13 r 13
+	duty HI_VOL
+    beat d E1
+
+    ;duty HI_VOL
+    beat a S3+T7
+    duty LO_VOL
+    beat a T8
+
+    duty HI_VOL
+    beat a S1+T3
+    duty LO_VOL
+    beat a T4 r E2
 .endr
 ; Measure 17
 .rept 2
-	duty $0e
-    beat c 13
-.redefine NO_FIRST_VOL 1
-	volbeat b 13
-	duty $0e
-	volbeat b 13 r 13
+	duty HI_VOL
+    beat c E1
+
+    ;duty HI_VOL
+    beat b S3+T7
+    duty LO_VOL
+    beat b T8
+
+    duty HI_VOL
+    beat b S1+T3
+    duty LO_VOL
+    beat b T4 r E2
 .endr
 ; Measure 18
 .rept 2
-	duty $0e
-    beat e 13
-.redefine NO_FIRST_VOL 1
-	volbeat b 13 r 13
-	duty $0e
-	volbeat b 13
+	duty HI_VOL
+    beat e E1
+
+    ;duty HI_VOL
+    beat b S3+T7
+    duty LO_VOL
+    beat b T8 r E1
+
+    duty HI_VOL
+    beat b S1+T3
+    duty LO_VOL
+    beat b T4
 .endr
 ; Measure 19
-	duty $0e
-    beat e 13
-	volbeat b 13
-.redefine NO_FIRST_VOL 0	
-	volbeat b 13 b 13
-	volbeat b 13 r 13
-	duty $0e
-	beat b 10 r 16
+	duty HI_VOL
+    beat e E1
 
-    goto lostWoodsChannel4Measure2Reset
+    ;duty HI_VOL
+    beat b S3+T7
+    duty LO_VOL
+    beat b T8
+
+    duty HI_VOL
+    beat b S1+T3
+    duty LO_VOL
+    beat b T4 
+
+    duty HI_VOL
+    beat b S3+T7
+    duty LO_VOL
+    beat b T8
+
+    duty HI_VOL
+    beat b S1+T3
+    duty LO_VOL
+    beat b T4 r E2
+
+	duty HI_VOL
+	beat b S1+T3 r T4+E2
+
+    goto musLostWoodsChannel4Measure2Loop
     cmdff
 
-
-
-lostWoodsChannel6:
-;$2a = tambourine
-;$24 = snare
-.redefine BEAT 1
+musLostWoodsChannel6:
+.redefine TAMB $2a
+.redefine SNARE $24
+.redefine HI_VOL $4
+.redefine LO_VOL $2
 ; Measure 1
-	vol $5
+	vol HI_VOL+1
 .rept 2
-    beat $2a 13 $2a 7 $2a 6
+    beat TAMB E1 TAMB S3 TAMB S4
 .endr
-    beat $2a 13 $2a 13
-    beat r 13 $24 13
+    beat TAMB E1 TAMB E2
+    beat r E1 SNARE E2
 
-lostWoodsChannel6Measure2Reset:
-    resetCoda
-lostWoodsChannel6Measure2:
-    incCoda
+musLostWoodsChannel6Measure2Loop:
+	vol HI_VOL
+.rept 8
 ; Measure 2-9
-	vol $4
 .rept 2
-    beat $2a 13 $2a 7 $2a 6
+    beat TAMB E1 TAMB S3 TAMB S4
 .endr
-    beat $2a 13 $2a 13
-    beat $24 13 $2a 13
+    beat TAMB E1 TAMB E2
+    beat SNARE E1 TAMB E2
+.endr
 
-    gotoCond 8 lostWoodsChannel6Measure10
-    goto lostWoodsChannel6Measure2  
+.rept 16 ; 4 measures
+; Measure 10-13
+    beat TAMB E1 TAMB S3 TAMB S4
+.endr 
 
-lostWoodsChannel6Measure10:
-    incCoda
-; Measure 10-13,18
-    beat $2a 13 $2a 7 $2a 6
-
-    gotoCond 8+16 lostWoodsChannel6Measure14
-    gotoCond 4+32 lostWoodsChannel6Measure19
-    goto lostWoodsChannel6Measure10    
-
-lostWoodsChannel6Measure14:
-    incCoda
+.rept 4
 ; Measure 14-15
-    beat $2a 13 $2a 7 $2a 6
-    beat $24 13 $2a 7 $2a 6
+    beat TAMB E1 TAMB S3 TAMB S4
+    beat SNARE E1 TAMB S3 TAMB S4
+.endr
 
-    gotoCond 4+24 lostWoodsChannel6Measure16
-    goto lostWoodsChannel6Measure14    
-
-lostWoodsChannel6Measure16:
-    incCoda
+.rept 4
 ; Measure 16-15
-    beat $2a 13 $2a 7 $2a 6
-    beat $24 26
+    beat TAMB E1 TAMB S3 TAMB S4
+    beat SNARE Q
+.endr
 
-    gotoCond 4+28 lostWoodsChannel6Measure10
-    goto lostWoodsChannel6Measure16  
+.rept 4
+; Measure 18
+    beat TAMB E1 TAMB S3 TAMB S4
+.endr 
 
-lostWoodsChannel6Measure19:
 ; Measure 19
-    beat r 13 $2a 13 $2a 13 $2a 13
-    beat $2a 26 $24 10 r 16
+    beat r E1 TAMB E2 TAMB E1 TAMB E2
+    beat TAMB Q SNARE S1+T3 r T4+E2
 
-    goto lostWoodsChannel6Measure2Reset
+    goto musLostWoodsChannel6Measure2Loop
     cmdff
+
+.undefine HI_VOL
+.undefine LO_VOL
+.undefine TAMB
+.undefine SNARE
